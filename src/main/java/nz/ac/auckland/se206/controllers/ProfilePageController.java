dbfbb2abc9db1d92f878179bbe3c863d3c7f0543
Kai <khir829@aucklanduni.ac.nz>
2022-09-08 18:32:45 +1200

    feat: Add profile page controller class

---
+++ src/main/java/nz/ac/auckland/se206/controllers/ProfilePageController.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ProfilePageController {
+  @FXML
+  private Label gamesLostLabel;
+  @FXML
+  private Label gamesWonLabel;
+  @FXML
+  private Label usernameLabel;
+
+
+}
