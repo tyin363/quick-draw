70e1684e413660e20abaaff1c27dc115523707b9
Tony Yin <tyin363@aucklanduni.ac.nz>
2022-09-10 19:27:27 +1200

    feat: Added a switch user controller class

---
+++ src/main/java/nz/ac/auckland/se206/controllers/SwitchUserController.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import nz.ac.auckland.se206.annotations.Singleton;
+
+@Singleton
+public class SwitchUserController {
+
+  @FXML
+  private void onCreateNewUser() {}
+}
