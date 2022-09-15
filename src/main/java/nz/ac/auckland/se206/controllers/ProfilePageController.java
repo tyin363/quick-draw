0174523a28090d3c0d7dc0c19847d366d0ad2756
Kai <khir829@aucklanduni.ac.nz>
2022-09-15 19:20:16 +1200

    feat: load in user profile to profile page
    If there are no users available, a new user is created with name 'new user'

--- src/main/java/nz/ac/auckland/se206/controllers/ProfilePageController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ProfilePageController.java
@@ -12,6 +12,7 @@ import nz.ac.auckland.se206.controllers.scenemanager.SceneManager;
 import nz.ac.auckland.se206.controllers.scenemanager.View;
 import nz.ac.auckland.se206.controllers.scenemanager.listeners.LoadListener;
 import nz.ac.auckland.se206.users.User;
+import nz.ac.auckland.se206.users.UserService;
 
 @Singleton
 public class ProfilePageController implements LoadListener {
@@ -23,7 +24,9 @@ public class ProfilePageController implements LoadListener {
   @FXML private TextField usernameTextField;
   @FXML private HBox usernameHbox;
 
-  @Inject private User user;
+  @Inject private UserService userService;
+
+  private User user;
 
   /** Switch to Main menu so user can choose what to do from there */
   @FXML
@@ -48,6 +51,22 @@ public class ProfilePageController implements LoadListener {
   @Override
   public void onLoad() {
     System.out.println("Profile Page loaded");
+    /*
+     * Temporary method to get user from the saved users. The first user in the list will be
+     * retrieved.
+     *
+     * If there are no users, a new user is created
+     *
+     * A way to get the 'current' user is to be implemented in the future
+     */
+    System.out.println(userService.getUsers());
+    if (userService.getUsers().isEmpty()) {
+      User newUser = new User("New user");
+      userService.saveUser(newUser);
+      user = newUser;
+    } else {
+      user = userService.getUsers().get(0);
+    }
     usernameHbox.setVisible(false);
   }
 }
