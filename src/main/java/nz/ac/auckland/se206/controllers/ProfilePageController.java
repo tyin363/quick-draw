37c7bdfceaa16b05b3c64afa5c867556f75fb82e
Kai <khir829@aucklanduni.ac.nz>
2022-09-15 20:05:34 +1200

    feat: add past words label on GUI

--- src/main/java/nz/ac/auckland/se206/controllers/ProfilePageController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ProfilePageController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import nz.ac.auckland.se206.annotations.Inject;
 import nz.ac.auckland.se206.annotations.Singleton;
 import nz.ac.auckland.se206.controllers.scenemanager.SceneManager;
@@ -24,6 +25,7 @@ public class ProfilePageController implements LoadListener {
   @FXML private Button switchAccountButton;
   @FXML private TextField usernameTextField;
   @FXML private HBox usernameHbox;
+  @FXML private VBox pastWordsVbox;
 
   @Inject private UserService userService;
 
