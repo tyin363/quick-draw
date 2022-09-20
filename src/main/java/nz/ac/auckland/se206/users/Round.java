7539fb41b126caf4bed45aebdc3fe95501075e18
pumbas600 <joshjeffers600@gmail.com>
2022-09-20 13:44:32 +1200

    feat: Create Round class for storing round stats

---
+++ src/main/java/nz/ac/auckland/se206/users/Round.java
@@ -0,0 +1,42 @@
+package nz.ac.auckland.se206.users;
+
+public class Round {
+
+  private String word;
+  private int timeTaken;
+  private boolean wasGuessed;
+
+  public Round(final String word, final int timeTaken, final boolean wasGuessed) {
+    this.word = word;
+    this.timeTaken = timeTaken;
+    this.wasGuessed = wasGuessed;
+  }
+
+  /**
+   * Retrieve the word that was being drawn in this round.
+   *
+   * @return The word that was being drawn in this round
+   */
+  public String getWord() {
+    return this.word;
+  }
+
+  /**
+   * Retrieve the time taken in seconds to draw the word in this round. If the word wasn't guessed,
+   * this will always be the maximum time.
+   *
+   * @return The time taken in seconds to draw the word in this round
+   */
+  public int getTimeTaken() {
+    return this.timeTaken;
+  }
+
+  /**
+   * If the word was guessed in the round.
+   *
+   * @return If the word was guessed in the round
+   */
+  public boolean isWasGuessed() {
+    return this.wasGuessed;
+  }
+}
