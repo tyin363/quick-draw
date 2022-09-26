28adca25873d369a1b39ccda6c7cd9e59f1ab785
pumbas600 <joshjeffers600@gmail.com>
2022-09-27 03:01:42 +1300

    docs: Add javadocs to method in Helpers

--- src/main/java/nz/ac/auckland/se206/util/Helpers.java
+++ src/main/java/nz/ac/auckland/se206/util/Helpers.java
@@ -6,6 +6,13 @@ import javafx.scene.layout.HBox;
 
 public class Helpers {
 
+  /**
+   * Given the header referenced by {@link nz.ac.auckland.se206.controllers.HeaderController}, it
+   * will iterate through the known hierarchy to find the back button.
+   *
+   * @param header The header root element
+   * @return The back button reference
+   */
   public static Button getBackButton(final AnchorPane header) {
     HBox hbox = (HBox) header.getChildren().get(0);
     final AnchorPane anchorPane = (AnchorPane) hbox.getChildren().get(0);
