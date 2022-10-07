ba071dcc213682ace2de368dd34233ff56fd3399
Kai <khir829@aucklanduni.ac.nz>
2022-10-07 19:32:27 +1300

    refactor: remove unused method for code style

--- src/main/java/nz/ac/auckland/se206/controllers/CanvasController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CanvasController.java
@@ -20,8 +20,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -290,19 +288,6 @@ public class CanvasController implements LoadListener, TerminationListener {
     if (wasGuessed) {
       this.gameOver(true);
     }
-  }
-
-  /**
-   * This method plays a sound effect given the file location of the sound effect and its volume.
-   *
-   * @param soundLocation The location of the sound file
-   * @param volume The volume of the sound effect
-   */
-  private void playSound(String soundLocation, double volume) {
-    Media soundEffect = new Media(new File(soundLocation).toURI().toString());
-    MediaPlayer mediaPlayer = new MediaPlayer(soundEffect);
-    mediaPlayer.setVolume(volume);
-    mediaPlayer.play();
   }
 
   /**
