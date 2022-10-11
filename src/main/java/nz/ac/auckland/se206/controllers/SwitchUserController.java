886a3fb55a4769cf96c9e3f4b3af4aec2ea6ab0a
Kai <khir829@aucklanduni.ac.nz>
2022-10-11 19:09:22 +1300

    feat: add click sound to switch user page

--- src/main/java/nz/ac/auckland/se206/controllers/SwitchUserController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/SwitchUserController.java
@@ -14,6 +14,7 @@ import nz.ac.auckland.se206.controllers.scenemanager.listeners.LoadListener;
 import nz.ac.auckland.se206.users.User;
 import nz.ac.auckland.se206.users.UserService;
 import nz.ac.auckland.se206.util.Config;
+import nz.ac.auckland.se206.util.SoundEffect;
 
 @Singleton
 public class SwitchUserController implements LoadListener {
@@ -21,6 +22,7 @@ public class SwitchUserController implements LoadListener {
   @Inject private SceneManager sceneManager;
   @Inject private UserService userService;
   @Inject private Config config;
+  @Inject private SoundEffect soundEffect;
 
   @FXML private GridPane userGrid;
   @FXML private Button newUser;
@@ -51,6 +53,7 @@ public class SwitchUserController implements LoadListener {
       // Setting current user and switching to main menu when user profile is clicked
       profile.setOnMouseClicked(
           event -> {
+            this.soundEffect.playClickSound();
             this.userService.setCurrentUser(user);
             this.sceneManager.switchToView(View.MAIN_MENU);
           });
@@ -72,6 +75,8 @@ public class SwitchUserController implements LoadListener {
    */
   @FXML
   private void onAddUser() {
+    this.soundEffect.playClickSound();
+
     final List<User> users = this.userService.getUsers();
     int newUserValue = 1;
 
