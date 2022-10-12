6e4f390ad2da889c34ebea6ee51635a6bbeb07bb
pumbas600 <joshjeffers600@gmail.com>
2022-10-13 03:56:27 +1300

    refactor: Setup client, server and core submodules

---
+++ client/src/main/java/nz/ac/auckland/se206/client/App.java
@@ -0,0 +1,28 @@
+package nz.ac.auckland.se206.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import nz.ac.auckland.se206.client.controllers.scenemanager.ApplicationContext;
+import nz.ac.auckland.se206.client.controllers.scenemanager.View;
+
+/**
+ * This is the entry point of the JavaFX application, while you can change this class, it should
+ * remain as the class that runs the JavaFX application.
+ */
+public class App extends Application {
+
+  public static void main(final String[] args) {
+    launch();
+  }
+
+  /**
+   * This method is invoked when the application starts. It initialises the scene manager with the
+   * starting scene.
+   *
+   * @param stage The primary stage of the application
+   */
+  @Override
+  public void start(final Stage stage) {
+    ApplicationContext.start(stage, View.SWITCH_USER);
+  }
+}
