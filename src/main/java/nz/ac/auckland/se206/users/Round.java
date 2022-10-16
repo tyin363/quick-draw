bc8e27180bad77e0d8bfeb138f642eb67bed6cf2
Tony <tyin363@aucklanduni.ac.nz>
2022-10-16 23:07:01 +1300

    refactor: Removed unused import from round class (actually)

--- src/main/java/nz/ac/auckland/se206/users/Round.java
+++ src/main/java/nz/ac/auckland/se206/users/Round.java
@@ -1,7 +1,6 @@
 package nz.ac.auckland.se206.users;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import nz.ac.auckland.se206.users.Round.Mode;
 
 public record Round(
     String word, int timeTaken, @JsonProperty("wasGuessed") boolean wasGuessed, Mode mode) {
