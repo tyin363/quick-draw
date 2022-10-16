32c2a5888dbedbf119060ae1f0d41e4ee1c81858
pumbas600 <joshjeffers600@gmail.com>
2022-10-17 02:47:33 +1300

    docs: Add javadocs to App and DashboardController

--- server/src/main/java/nz/ac/auckland/se206/server/controllers/DashboardController.java
+++ server/src/main/java/nz/ac/auckland/se206/server/controllers/DashboardController.java
@@ -19,15 +19,21 @@ public class DashboardController {
   @FXML private Button startSession;
   @FXML private Label onlineStudents;
 
+  /**
+   * Set the initial number of online students and add a listener to update it as new clients join.
+   */
   @FXML
   private void initialize() {
     this.onlineStudents.setText(Integer.toString(this.server.clientCount()));
+    // This listener is called on a different thread, so we need to use Platform.runLater to
+    // allow us to update the UI.
     this.server.addClientCountChangeListener(
         () ->
             Platform.runLater(
                 () -> this.onlineStudents.setText(Integer.toString(this.server.clientCount()))));
   }
 
+  /** Starts a new drawing session with the target word specified in the text field. */
   @FXML
   private void onStartSession() {
     this.server.startDrawingSession(this.targetWord.getText().toLowerCase(Locale.ROOT));
@@ -35,6 +41,7 @@ public class DashboardController {
     this.startSession.setDisable(true);
   }
 
+  /** Only allow you to start a new session if the target word is not blank. */
   @FXML
   private void onChange() {
     this.startSession.setDisable(this.targetWord.getText().isBlank());
